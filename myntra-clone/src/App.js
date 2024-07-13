1b74cd67fa38e9833a91f583f5d425e22bfe2853
Prajakta Kapoor <kprajakta235@gmail.com>
2024-07-13 19:44:10 +0530

    Update App.js

--- myntra-clone/src/App.js
+++ myntra-clone/src/App.js
@@ -1,46 +1,59 @@
 import React from "react";
-import './App.css';
+import "./App.css";
+import "./Customization/Styles.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./Components/Header";
-// import Feed from "./Components/Feed";
-import Home from "./Components/Home";
+import Home from "./Customization/Home.jsx";
 import Checkout from "./Components/Checkout";
 import Login from "./Components/Login";
 import Ioa from "./Components/Ioa";
+
 function App() {
   return (
     <Router>
       <div className="App">
         <Routes>
-         <Route path="/checkout" element={
-            <>
-              <Header />
-              <Checkout />
-            </>
-            } />
-
-          <Route path="/login" element={
-            <>
-              <Login />
-            </>
-          } />
-          
-          <Route path="/" element={
-            <>
-              <Header />
-              <Home />
-            </>
-          } />
-        
-            <Route path="/posts" element={
+          <Route
+            path="/checkout"
+            element={
+              <>
+                <Header />
+                <Checkout />
+              </>
+            }
+          />
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/"
+            element={
+              <>
+                <Header />
+                <Home />
+              </>
+            }
+          />
+          <Route
+            path="/posts"
+            element={
               <>
                 <Header />
                 <Ioa />
               </>
-            } />
-        </Routes> 
+            }
+          />
+          <Route
+            path="/customization"
+            element={
+              <>
+                <Header />
+                <Home />
+              </>
+            }
+          />
+        </Routes>
       </div>
     </Router>
   );
 }
+
 export default App;
